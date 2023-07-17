8e1c09a0d655031ba34db03d5ab781774b52885b
caneta <canduc17@gmail.com>
2023-07-17 10:43:00 +0200

    Add plugin to comment jsx/tsx propery
    [${ROAD_ID} | ${JIRA_ID}]

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -30,7 +30,19 @@ local plugins = {
 
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
     opts = overrides.treesitter,
+  },
+  {
+    "numToStr/Comment.nvim",
+    dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
+    config = function()
+      require("Comment").setup {
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      }
+    end,
   },
 
   {
